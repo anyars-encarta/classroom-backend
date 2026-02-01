5d96d40706242b9810518da68219039ed420c0a0
Anyars Yussif <anyarsencarta@gmail.com>
2026-02-01 12:05:20 +0000

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
 app.use(express.json());
 
