1a6d6f6dcf16eb91b70aa96a08e566727b63a18d
Anyars Yussif <anyarsencarta@gmail.com>
2026-02-07 14:41:51 +0000

    Implement codeRabbit suggested changes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,12 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
 
+const secret = process.env.BETTER_AUTH_SECRET!;
+
+if(!secret) throw new Error("BETTER_AUTH_SECRET is not set in the .env file");
+
 export const auth = betterAuth({
-    secret: process.env.BETTER_AUTH_SECRET!,
+    secret,
     trustedOrigins: [process.env.FRONTEND_URL!],
     database: drizzleAdapter(db, {
         provider: "pg",
@@ -16,7 +20,7 @@ export const auth = betterAuth({
     user: {
         additionalFields: {
             role: {
-                type: "string", required: true, defaultValue: "student", input: true,
+                type: "string", required: true, defaultValue: "student", input: false,
             },
             imageCldPubId: {
                 type: "string", required: false, input: true,
