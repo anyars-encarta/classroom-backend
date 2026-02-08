0197ee7de5da719bd82ef7dd0eefe8898f97959d
Anyars Yussif <anyarsencarta@gmail.com>
2026-02-08 12:43:30 +0000

    Add baseURL in betterAuth config and use FRONTEND_URL as variable name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,13 +4,14 @@ import { db } from "../db/index.js";
 import * as schema from "../db/schema/auth.js";
 
 const secret = process.env.BETTER_AUTH_SECRET!;
-const frontendUrl = process.env.BETTER_AUTH_BASE_URL!;
+const frontendUrl = process.env.FRONTEND_URL!;
 
 if(!secret) throw new Error("BETTER_AUTH_SECRET is not set in the .env file");
-if(!frontendUrl) throw new Error("BETTER_AUTH_BASE_URL is not set in the .env file");
+if(!frontendUrl) throw new Error("FRONTEND_URL is not set in the .env file");
 
 export const auth = betterAuth({
     secret,
+    baseURL: frontendUrl,
     trustedOrigins: [frontendUrl],
     database: drizzleAdapter(db, {
         provider: "pg",
